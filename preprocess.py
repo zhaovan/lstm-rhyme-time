14f26b3ef39f244e5827edc60ebec6879f39bcfa
Sorin Cho <sorincho@gmail.com>
2019-12-01 22:07:28 -0500

    cleanup preprocess

--- preprocess.py
+++ preprocess.py
@@ -6,58 +6,57 @@ import pronouncing
 
 def get_data(file_path):
     # add any characters to this set if you don't want it in the final data
-    exclude = set(['!', '.', ',', '?'])
-    def flatten(l): return [item for sublist in l for item in sublist]
+    exclude = set(['!', '.', ',', '?', "'", '"'])
+    special_chars = set(["NEWLINE", "SPACE", "END"])
+    # def flatten(l): return [item for sublist in l for item in sublist]
+    unique_syllables = set([])
     with open(file_path) as f:
         csv_reader = csv.reader(f)
         data = []
         for row in csv_reader:
-            # get rid of special chacacters and punctuation
             row[0] = "".join(c for c in row[0] if c not in exclude)
-            # split by line
-            poem = (row[0].split("\n"))[:-1]
-            # double check the poem has 5 lines
-            if len(poem) == 5:
-                # each line is now split into words
-                for i in range(5):
-                    poem[i] = poem[i].split(" ")
-                    # for each word:
-                    for w in range(len(poem[i])):
-                        # search word in cmudict
-                        p_list = pronouncing.phones_for_word(poem[i][w])
-                        # if syllables exist in dict, get the first one
-                        if(len(p_list) > 0):
-                            poem[i][w] = p_list[0]
-                        # else, make an UNK syllable list of length
-                        # where length is word length divided by 3
-                        # [:-1] removes last space in UNK list
-                        else:
-                            poem[i][w] = (
-                                "UNK " * int(len(poem[i][w]) / 3))[:-1]
 
-                        # split the word by syllables
-                        if w == (len(poem[i]) - 1):
-                            # if last line, and last word, add END
-                            if(i == 4):
-                                poem[i][w] = (poem[i][w] + " END").split(" ")
-                            # if not last line, but last word, add NEWLINE
-                            else:
-                                poem[i][w] = (
-                                    poem[i][w] + " NEWLINE").split(" ")
-                        # if not last line or last word, add SPACE
-                        else:
-                            poem[i][w] = (poem[i][w] + " SPACE").split(" ")
-                    # make syllable word list into just a syllable list
-                    # essentially, flatten the list
-                    poem[i] = [item for sublist in poem[i]
-                               for item in sublist]
-                # flatten the list of newlines
-                poem = [item for sublist in poem
-                        for item in sublist]
-                data.append(poem)
-        print(data[0])
-        print(len(data))
-        return data
+            row[0] = row[0].replace(" ", " SPACE ")
+            row[0] = (row[0].replace("\n", " NEWLINE "))
+            row[0] = row[0].split(" ")
+            poem = []
+            for w in row[0]:
+                w = w.strip("'")
+                w = w.strip('"')
+                w = w.strip("(")
+                w = w.strip(")")
+                if w != "":
+                    poem.append(w)
+            poem[-1] = "END"
+            data += poem
+        print(data[:200])
+        syllables = []
+        for w in data:
+            if w not in special_chars:
+                p_list = pronouncing.phones_for_word(w)
+                # if syllables exist in dict, get the first one
+                syllable_word = ""
+                if(len(p_list) > 0):
+                    syllable_word = p_list[0]
+                    syllable_list = syllable_word.split(" ")
+                # else, make an UNK syllable list of length
+                # where length is word length divided by 3
+                # [:-1] removes last space in UNK list
+                else:
+                    syllable_list = ["UNK" for i in range(int(len(w) / 3))]
+                    # syllable_word = ("UNK " * int(len(w) / 3))[:-1]
+                syllables += syllable_list
+            else:
+                syllables.append(w)
+        print(syllables[:200])
+        unique_syllables = set(syllables)
+        syllable_dict = {}
+        i = 0
+        for s in unique_syllables:
+            syllable_dict[i] = s
+            i += 1
+        print(syllable_dict)
+        return syllables, syllable_dict, len(syllable_dict)
 
 
 if __name__ == "__main__":
