be2856fa8aa6d391fb0856f3d3051df17207caba
TimothyWang56 <timothywang56@gmail.com>
2019-12-15 18:15:12 -0500

    final edits to model and preprocess

--- model.py
+++ model.py
@@ -1,23 +1,26 @@
 import numpy as np
 import tensorflow as tf
 from preprocess import *
+import copy
 
 
 class PoemGenerator(tf.keras.Model):
     def __init__(self, vocab_size):
         super(PoemGenerator, self).__init__()
 
-        # Numerical Hyperparameters
+        # hyper parameters
         self.batch_size = 128
         self.window_size = 150
         self.learning_rate = 0.001
         self.rnn_size = 128
+        # both hidden size and embedding size were recommended to be 256 by the paper
         self.hidden_size = 256
         self.embedding_size = 256
         self.vocab_size = vocab_size
         self.optimizer = tf.keras.optimizers.Adam(
             learning_rate=self.learning_rate)
 
+        # instantiate layers for our model
         self.embedding_layer = tf.keras.layers.Embedding(
             self.vocab_size, self.embedding_size, input_length=self.window_size)
         self.rnn_layer = tf.keras.layers.LSTM(
@@ -28,38 +31,41 @@ class PoemGenerator(tf.keras.Model):
             self.vocab_size)
 
     def call(self, inputs, initial_state):
+        # architecture from our paper - embedding layer, LSTM layer, and then 2 dense layers before returning logits
         embeddings = self.embedding_layer(tf.convert_to_tensor(inputs))
         (output, last_output, cell_state) = self.rnn_layer(embeddings, initial_state)
         logits = self.dense_1(output)
         logits = self.dense_2(logits)
-        # logits = self.dense(output)
         return logits, last_output, cell_state
 
     def loss(self, logits, labels):
+        # sparse categorical crossentropy with logits because we don't softmax in our call function
         return tf.math.reduce_mean(tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True))
-
 
 def train(model, train_inputs, train_labels, epoch):
     i = 0
     end = model.batch_size
     length = len(train_labels)
-    # indices = tf.random.shuffle([l for l in range(length)])
-    # train_inputs = tf.gather(train_inputs, indices)
-    # train_labels = tf.gather(train_labels, indices)
-    previous_state = None
+    # shuffle poems for better results
+    indices = tf.random.shuffle([l for l in range(length)])
+    train_inputs = tf.gather(train_inputs, indices)
+    train_labels = tf.gather(train_labels, indices)
+    batch = 0
+    # loop through all the poems and update gradients
     while end <= length:
         batch_inputs = train_inputs[i:end]
         batch_labels = train_labels[i:end]
         with tf.GradientTape() as tape:
-            logits, last_output, previous_cell_state = model(batch_inputs, previous_state)
-            previous_state = (last_output, previous_cell_state)
+            logits, _, _ = model(batch_inputs, None)
             loss = model.loss(logits, batch_labels)
+            if batch % 100 == 0:
+                print(loss)
+            batch += 1
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(
             zip(gradients, model.trainable_variables))
         i += model.batch_size
         end += model.batch_size
-
 
 def test(model, test_inputs, test_labels):
     i = 0
@@ -76,79 +82,78 @@ def test(model, test_inputs, test_labels):
         i += model.batch_size
         end += model.batch_size
 
+    # return perplexity
     return tf.math.exp(loss / avg)
 
 
-def generate_sentence(word1, length, vocab, model):
+def generate_poem(word1, vocab, model):
     reverse_vocab = {idx: word for word, idx in vocab.items()}
     previous_state = None
-
     first_string = word1
     first_word_index = vocab[word1]
     next_input = [[first_word_index]]
     text = [first_string]
 
-    for _ in range(length):
+    # for loop to generate next 200 phonemes, or the END token
+    for _ in range(200):
         logits, last_output, cell_state = model.call(next_input, previous_state)
         previous_state = (last_output, cell_state)
-        # out_index = np.argmax(np.array(logits[0][0]))
+
         logits = tf.squeeze(logits, 0)
+        # weighted randomization of the next phoneme
         out_index = tf.random.categorical(logits, num_samples=1)[-1, 0].numpy()
         
+        next_phoneme = reverse_vocab[out_index]
         text.append(reverse_vocab[out_index])
+
+        if next_phoneme == "END":
+            break
+
         next_input = [[out_index]]
 
     print(" ".join(text))
 
 
 def main():
-    (ids, syllable_dict, syllable_size) = get_data(
+    (ids, phoneme_dict, phoneme_size, pad_index) = get_data(
         "./data/limericks.csv")
-    
+
+    # first 80% of the poems are for training, the last 20% are for testing
     num_train_ids = int(0.8 * len(ids))
-    train_ids = ids[0:num_train_ids]
-    test_ids = ids[num_train_ids:]
 
-    train_inputs = train_ids[:-1]
-    train_labels = train_ids[1:]
+    label_ids = copy.deepcopy(ids)
 
-    test_inputs = test_ids[:-1]
-    test_labels = test_ids[1:]
-
-    model = PoemGenerator(syllable_size)
-
-    i = 0
-    end = model.window_size
-    train_length = len(train_labels)
-    train_inputs_setup = []
-    train_labels_setup = []
-    while end <= train_length:
-        train_inputs_setup.append(train_inputs[i:end])
-        train_labels_setup.append(train_labels[i:end])
-        i += model.window_size
-        end += model.window_size
-
-    i = 0
-    end = model.window_size
-    test_length = len(test_labels)
-    test_inputs_setup = []
-    test_labels_setup = []
-    while end <= test_length:
-        test_inputs_setup.append(test_inputs[i:end])
-        test_labels_setup.append(test_labels[i:end])
-        i += model.window_size
-        end += model.window_size
+    # cut off the first word of every poem and add a PAD token to the end of them for labels
+    for i in range(len(label_ids)):
+        l = label_ids[i]
+        new_l = l[1:]
+        new_l.append(pad_index)
+        label_ids[i] = new_l
     
+    # making training inputs and labels
+    train_inputs = ids[:num_train_ids]
+    train_labels = label_ids[:num_train_ids]
 
+    # making testing inputs and labels
+    test_inputs = ids[num_train_ids:]
+    test_labels = label_ids[num_train_ids:]
+
+    # instantiate model with phoneme_size
+    model = PoemGenerator(phoneme_size)
+
+    # train model for 15 epochs, printing perplexity after each epoch
     for i in range(15):
-        # print("EPOCH " + str(i + 1))
-        train(model, train_inputs_setup, train_labels_setup, i + 1)
-        perplexity = test(model, test_inputs_setup, test_labels_setup)
+        print("------------------------------")
+        print("EPOCH " + str(i + 1))
+        train(model, train_inputs, train_labels, i + 1)
+        perplexity = test(model, test_inputs, test_labels)
         print("PERPLEXITY OF EPOCH " + str(i + 1) + ": " + str(perplexity))
+
+    # while loop to generate poems starting with a specific phoneme
     while True:
-        query = input("Enter syllable: ")
+        query = input("Enter phoneme: ")
         try:
-            generate_sentence(query, 500, syllable_dict, model)
+            generate_poem(query, phoneme_dict, model)
         except:
             print("Invalid key. Type again")
 
--- preprocess.py
+++ preprocess.py
@@ -5,19 +5,25 @@ import pronouncing
 
 
 def get_data(file_path):
-    # add any characters to this set if you don't want it in the final data
+    # set of characters to exclude
     exclude = set(['!', '.', ',', '?', "'", '"', "#"])
+    # set of phonemes to exclude, since the CMU dict sometimes returns these words instead of phoenems
+    phoneme_exclude = set(["#", "foreign", "french", "abbrev"])
+    # set of special_chars that do NOT get converted to phonemes
     special_chars = set(["NEWLINE", "SPACE", "END"])
+    # open csv file
     with open(file_path) as f:
         csv_reader = csv.reader(f)
         data = []
         for row in csv_reader:
+            # get rid of characters from the exclude set
             row[0] = "".join(c for c in row[0] if c not in exclude)
-
+            # put in appropriate SPACE and NEWLINE tokens
             row[0] = row[0].replace(" ", " SPACE ")
             row[0] = (row[0].replace("\n", " NEWLINE "))
             row[0] = row[0].split(" ")
             poem = []
+            # strip special symbols
             for w in row[0]:
                 w = w.strip("'")
                 w = w.strip('"')
@@ -25,34 +31,53 @@ def get_data(file_path):
                 w = w.strip(")")
                 if w != "":
                     poem.append(w)
+            # put an END token at the end of a poem
             poem[-1] = "END"
-            data += poem
-        syllables = []
-        for w in data:
-            if w not in special_chars:
-                p_list = pronouncing.phones_for_word(w)
-                syllable_word = ""
-                if (len(p_list) > 0):
-                    syllable_word = p_list[0]
-                    syllable_list = syllable_word.split(" ")
-                # else, make an UNK syllable list of length
-                # where length is word length divided by 3
-                # [:-1] removes last space in UNK list
-                else:
-                    syllable_list = ["UNK" for i in range(int(len(w) / 3))]
-                final_syllable_list = [s for s in syllable_list if s != "#" and s != "foreign" and s != "french" and s != "abbrev"]
-                syllables += final_syllable_list
-            else:
-                syllables.append(w)
-        unique_syllables = set(syllables)
-        syllable_dict = {}
-        i = 0
-        for s in unique_syllables:
-            syllable_dict[s] = i
-            i += 1
-        syllables_indexes = [syllable_dict[s] for s in syllables]
-        return syllables_indexes, syllable_dict, len(syllable_dict)
+            data.append(poem)
 
+        phonemes_data = []
+        for poem in data:
+            poem_phonemes = []
+            # for each word in a poem, convert it to it's phoneme representation, unless it's a SPACE, NEWLINE, or END token
+            for word in poem:
+                if word not in special_chars:
+                    p_list = pronouncing.phones_for_word(word)
+                    phoneme_word = ""
+                    if (len(p_list) > 0):
+                        phoneme_word = p_list[0]
+                        phoneme_list = phoneme_word.split(" ")
+                    else:
+                        phoneme_list = ["UNK" for i in range(int(len(word)/ 2))]
+                    final_phoneme_list = [p for p in phoneme_list if p not in phoneme_exclude]
+                    poem_phonemes += final_phoneme_list
+                else:
+                    poem_phonemes.append(word)
+            poem_len = len(poem_phonemes)
+            # only include poems with phoneme length 120-150, so that the structure of our data remain consistent
+            if poem_len <= 150 and poem_len >= 120:
+                phonemes_data.append([poem_phonemes, poem_len])
+        padded_phonemes_data = []
+
+        # pad poems with PAD tokens so that they're all the same length (cannot feed into LSTM otherwise)
+        for (poem, poem_len) in phonemes_data:
+            padded_poem = poem 
+            padded_poem += ["PAD" for _ in range(150 - poem_len)]
+            padded_phonemes_data.append(padded_poem)
+
+        # set of unique phonemes
+        unique_phonemes = set([phoneme for poem in padded_phonemes_data for phoneme in poem])
+        
+        phonemes_dict = {}
+        i = 0
+
+        # create phoneme dict with unique ids for every phoneme (including special tokens END, SPACE, NEWLINE, and PAD)
+        for s in unique_phonemes:
+            phonemes_dict[s] = i
+            i += 1
+        phoneme_indexes = [[phonemes_dict[s] for s in poem] for poem in padded_phonemes_data]
+
+        # return the phoneme indexes, the dictionary, the length of the dictionary, and the unique ID of PAD
+        return phoneme_indexes, phonemes_dict, len(phonemes_dict), phonemes_dict["PAD"]
 
 if __name__ == "__main__":
     get_data("data/limericks.csv")
